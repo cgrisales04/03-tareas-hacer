98364a301543c42abe08fe1f7d8c0573ed308251
cgrisales04 <cristiangrisales04@gmail.com>
2023-05-02 17:04:32 -0500

    Opciones de menú interactivo - Inquirer

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-require("colors");
-
-const { mostrarMenu, pausa } = require("./helpers/mensajes.js");
-
+const { inquirerMenu, pausa } = require("./helpers/inquirer.js");
 console.clear();
 
 const main = async () => {
   try {
     let opt = "";
     do {
-      opt = await mostrarMenu();
-      if (opt !== "0") await pausa();
+      opt = await inquirerMenu();
+      console.log(opt);
+      if (opt == "0") await pausa();
     } while (opt !== "0");
   } catch (error) {
     console.log(error);
   }
-
-  //   console.log(opcion);
-  //   console.log(opcion);
-  //   do {
-  // opcion = mostrarMenu();
-  //   } while (opcion != 0);
-  //   pausa();
 };
 
 main();
---
+++ helpers/inquirer.js
@@ -0,0 +1,65 @@
+const inquirer = require("inquirer");
+require("colors");
+
+const preguntas = [
+  {
+    type: "list",
+    name: "opcion",
+    message: "¿Qué deseas hacer?",
+    choices: [
+      {
+        value: "1",
+        name: "1. Crear tarea",
+      },
+      {
+        value: "2",
+        name: "2. Listar tareas",
+      },
+      {
+        value: "3",
+        name: "3. Listar tareas completadas",
+      },
+      {
+        value: "4",
+        name: "4. Listar tareas pendientes",
+      },
+      {
+        value: "5",
+        name: "5. Completar tareas",
+      },
+      {
+        value: "6",
+        name: "6. Borrar tarea",
+      },
+      {
+        value: "0",
+        name: "0. Salir",
+      },
+    ],
+  },
+];
+
+const inquirerMenu = async () => {
+  console.clear();
+  console.log("======================".green);
+  console.log("Seleccione una opción".green);
+  console.log("======================".green);
+
+  const { opcion } = await inquirer.prompt(preguntas);
+  return opcion;
+};
+
+const pausa = async () => {
+  return await inquirer.prompt([
+    {
+      type: "input",
+      name: "salir",
+      message: `Presione ${"enter".green} para continuar.`,
+    },
+  ]);
+};
+
+module.exports = {
+  inquirerMenu,
+  pausa,
+};
